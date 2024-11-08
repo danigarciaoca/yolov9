f2c3f50985e07c4c7aed2d7594c5fb6a8ea8e459
danigarciaoca <danielman8@gmail.com>
2024-11-08 17:15:42 +0100

    webcam demo

---
+++ main_cam.py
@@ -0,0 +1,53 @@
+import time
+
+import cv2
+
+from yolov9.yolov9 import YOLOv9
+
+
+def main(det_model, video_source):
+    # If no video source is provided, use the default webcam
+    cap = cv2.VideoCapture(video_source)
+
+    # Check if the video capture has been successfully initialized
+    if not cap.isOpened():
+        print("Error: Could not open video source.")
+        return
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # If frame capture failed, break the loop
+        if not ret:
+            print("Error: Could not read frame.")
+            break
+
+        # run inference and draw detections
+        start = time.time()
+        det = det_model(frame, classes=None, conf_thres=0.25)[0]  # keep item 0 (single image inference)
+        fps = 1 / (time.time() - start)
+        print(f"FPS: {fps}")
+
+        frame_out = model.annotate(frame, det, line_width=3)
+
+        # Display the resulting frame
+        cv2.imshow('Webcam', frame_out)
+
+        # Exit if ESC key is pressed
+        if cv2.waitKey(1) == 27:  # 27 is the ASCII code for ESC
+            print("ESC key pressed. Exiting...")
+            break
+
+    # Release the webcam and close all OpenCV windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+# Run the main function
+if __name__ == "__main__":
+    # load model
+    weights_path = "weights/yolov9-e-converted.pt"  # model weights
+    model = YOLOv9(weights_path, half=False)
+
+    main(det_model=model, video_source=0)
