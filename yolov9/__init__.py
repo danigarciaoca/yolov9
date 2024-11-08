f24fc2b9a0cf979d42a6d3d7608397347f7e4fbf
danigarciaoca <danielman8@gmail.com>
2024-11-08 18:40:38 +0100

    simpler imports from __init__.py

--- yolov9/__init__.py
+++ yolov9/__init__.py
@@ -5,3 +5,7 @@ FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLO root directory
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
+
+from .yolov9 import YOLOv9
+
+__all__ = ["YOLOv9", "ROOT"]  # allow simpler import
