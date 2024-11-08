35a90558d9b6165f697f18166d0b98988f4aa8c8
danigarciaoca <danielman8@gmail.com>
2024-11-08 14:49:06 +0100

    Update YOLOv9 class

--- yolov9/yolov9.py
+++ yolov9/yolov9.py
@@ -4,8 +4,28 @@ import torch
 
 from models.common import DetectMultiBackend
 from utils.augmentations import letterbox
-from utils.general import check_img_size, non_max_suppression
+from utils.general import check_img_size, non_max_suppression, scale_boxes
+from utils.plots import colors
 from utils.torch_utils import select_device
+
+
+def box_label(im, box, label='', color=(128, 128, 128), lw=2, txt_color=(255, 255, 255)):
+    # Add one xyxy box to image with label
+    p1, p2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
+    cv2.rectangle(im, p1, p2, color, thickness=lw, lineType=cv2.LINE_AA)
+    if label:
+        tf = max(lw - 1, 1)  # font thickness
+        w, h = cv2.getTextSize(label, 0, fontScale=lw / 3, thickness=tf)[0]  # text width, height
+        outside = p1[1] - h >= 3
+        p2 = p1[0] + w, p1[1] - h - 3 if outside else p1[1] + h + 3
+        cv2.rectangle(im, p1, p2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(im,
+                    label, (p1[0], p1[1] - 2 if outside else p1[1] + h + 2),
+                    0,
+                    lw / 3,
+                    txt_color,
+                    thickness=tf,
+                    lineType=cv2.LINE_AA)
 
 
 class YOLOv9:
@@ -44,13 +64,19 @@ class YOLOv9:
                  ):
         """ Runs object detection on the input image. """
         # input image pre-processing
+        img_sz = img.shape[:2]  # input image size (before pre-processing)
         img = self.preprocess(img)
+        img_sz_p = img.shape[2:]  # input image size (after pre-processing)
 
         # inference
         pred = self.model(img, augment=augment, visualize=visualize)
 
         # NMS post-processing
         pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+        # Rescale boxes to original image size
+        for det in pred:  # per image (batch cases)
+            if len(det):
+                det[:, :4] = scale_boxes(img_sz_p, det[:, :4], img_sz).round()
 
         return pred
 
@@ -73,6 +99,20 @@ class YOLOv9:
 
         return im
 
+    def annotate(self,
+                 img,  # image to annotate
+                 det,  # detections used for annotation
+                 hide_labels=False,  # hide labels
+                 hide_conf=False  # hide confidences
+                 ):
+        for *xyxy, conf, cls in reversed(det):
+            # Draw bbox into the image
+            c = int(cls)  # integer class
+            label = None if hide_labels else (self.names[c] if hide_conf else f'{self.names[c]} {conf:.2f}')
+            box_label(img, xyxy, label, color=colors(c, True), lw=2)
+
+        return img
+
 
 if __name__ == "__main__":
     # base paths
@@ -87,5 +127,15 @@ if __name__ == "__main__":
 
     # run object detection
     det = model(img, classes=None, conf_thres=0.25, max_det=1000)[0]  # no batch inference, so just keep item 0
+
+    # annotate image with detections
+    img_out = model.annotate(img, det)
+
+    # show annotated image
+    cv2.imshow('Annotated image', img_out)
+
+    # wait until a key is pressed, then close the window
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
     print("Done!")
