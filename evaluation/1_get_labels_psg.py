2a9596c3f2d8c69d1ea73f08f0447d8a82576d3f
danigarciaoca <danielman8@gmail.com>
2024-11-14 17:06:11 +0100

    Tool to annotate a dataset by using YOLOv9 trained on Prosegur dataset

---
+++ evaluation/1_get_labels_psg.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+import cv2
+from tqdm import tqdm
+
+from yolov9 import YOLOv9
+
+
+def process_image(det_model, in_img_path, out_labels_path, view_img):
+    # load input image
+    img = cv2.imread(in_img_path)
+
+    # run object detection
+    det = det_model(img, classes=None, conf_thres=0.1, max_det=1000)[0]  # single image, so just keep item 0
+
+    # annotate image with detections
+    txt_out = out_labels_path / in_img_path.stem
+    img_out = model.annotate(img, det, line_width=3, annot_img=view_img,
+                             save_txt=True, save_conf=True, txt_file=txt_out)
+
+    if view_img:
+        # show annotated image
+        cv2.imshow('Annotated image', img_out)
+
+        # wait until a key is pressed, then close the window
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    # input images folder
+    images_dir = Path("coco/test/images")  # folder containing input images to be processed
+
+    # output predicted labels path (class + bbox)
+    labels_path = Path("coco_test_yolov9_psg/labels/test")
+    labels_path.mkdir(parents=True, exist_ok=True)
+
+    # load model
+    weights_path = "/home/danigarciaoca/PycharmProjects/yolov9/yolov9/src/runs/train/exp/weights/best.pt"  # model weights
+    model = YOLOv9(weights_path, half=True)
+
+    # run inference
+    for img_path in tqdm(images_dir.iterdir(), total=2279):
+        process_image(det_model=model, in_img_path=img_path, out_labels_path=labels_path, view_img=True)
+
+    print("Done!")
